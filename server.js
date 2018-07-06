2432b06d7af14070386970b38c7bb598429d37f4
nataliestam <natalielynnstam@gmail.com>
2018-07-05 19:14:30 -0700

    add all services

--- server.js
+++ server.js
@@ -1,71 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const axios = require('axios');
-const port = 3000;
-const request = require('request');
+const port = 3006;
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/:id', express.static('public'));
+app.use('/product/:id', express.static('public'));
 app.use(bodyParser.json());
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
 });
 
-
 app.get('/productDetails/:id', (req, res) => {
-  // console.log(`http://localhost:3001${req.url}`)
-  axios.get(`http://18.219.25.187${req.url}`)
-    .then((response) => {
-      console.log('this is the response', response.data)
-      res.send(response.data);
-    })
-    .catch((err) => {
-      console.log('your axios has an error', err)
-    });
+  res.redirect(`http://ec2-54-219-171-140.us-west-1.compute.amazonaws.com${req.url}`);
 });
 
-app.get('/reviews-module/reviews/:id', (req, res) => {
-  console.log(`http://54.183.143.240${req.url}`);
-  request(`http://54.183.143.240${req.url}`, (error, response, body) => {
-    if (error) throw new Error(error);
-    res.end(body);
-  });
+app.get('/reviews-module/reviews/product/:id', (req, res) => {
+  res.redirect(`http://18.191.180.179${req.url}`);
 });
 
-app.put('/reviews-module/reviews', (req, res) => {
-  console.log('req.body', req.body);
-  axios.put(`http://54.183.143.240${req.url}`, req.body)
-    .then(() => {
-      res.status(204).end();
-    })
-    .catch((err) => {
-      throw (err);
-    });
+app.get('/product/:id/related', (req, res) => {
+  res.redirect(`http://you-may-also-like-load-balancer-1474842517.us-west-1.elb.amazonaws.com${req.url}`);
 });
 
-app.get('/youMayAlsoLike/:id', (req, res) => {
-  axios.get(`http://18.188.58.255${req.url}`)
-    .then((response) => {
-      console.log('this is the response', response.data)
-      res.send(response.data);
-    })
-    .catch((err) => {
-      console.log('your axios has an error', err)
-    });
+app.get('/product/:id/images', (req, res) => {
+  res.redirect(`http://ec2-18-219-118-99.us-east-2.compute.amazonaws.com${req.url}`);
 });
-
-app.get('/images/:id', (req, res) => {
-  axios.get(`http://138.197.208.221${req.url}`)
-    .then((response) => {
-      console.log('this is the response', response.data)
-      res.send(response.data);
-    })
-    .catch((err) => {
-      console.log('your axios has an error', err)
-    });
-});  
-
